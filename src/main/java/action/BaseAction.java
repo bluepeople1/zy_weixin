acefb9601e32ca4b1c0ca96ea6edbb44cd67d284
caizhongrui <qdzy_cai@163.com>
2015-05-14 19:40:29 +0800

    初始化系统架构

--- src/main/java/action/BaseAction.java
+++ src/main/java/action/BaseAction.java
@@ -1,13 +1,17 @@
 package action;
 
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
- * Created by caizhongrui on 15/5/11.
+ * Created by caizhongrui on 15/5/14.
  * ==============================================
  * 类说明：
  * ==============================================
- * 蔡仲瑞          15/5/11             创建
+ * 蔡仲瑞          15/5/14             创建
  * ==============================================
+ * 青岛众应信息科技有限公司版权所有
  * ==============================================
  */
-public class TestAction extends BaseAction{
+public class BaseAction extends ActionSupport{
+
 }
---
+++ src/main/java/dao/BaseDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ * Created by caizhongrui on 15/5/14.
+ * ==============================================
+ * 类说明：
+ * ==============================================
+ * 蔡仲瑞          15/5/14             创建
+ * ==============================================
+ * 青岛众应信息科技有限公司版权所有
+ * ==============================================
+ */
+public class BaseDAO extends HibernateDaoSupport {
+}
